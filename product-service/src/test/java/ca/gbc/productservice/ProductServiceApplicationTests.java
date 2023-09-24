25985d3af25e7b2da2bb81a1d3c779e45edce71e
unknown <ferielmaamer@yahoo.fr>
2023-09-23 23:05:59 -0400

    final changes

--- product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package ca.gbc.microservicesparent;
+package ca.gbc.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroservicesParentApplicationTests {
+class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
